8d67dce57824024509fa892ad42371b7ab05624c
geosoft1 <geosoft1@gmail.com>
2019-05-03 11:34:27 +0300

    -

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 
 // TODO application version
 // https://semver.org/#semantic-versioning-200
-const SW_VERSION = "1.1.0-release-build300419"
+const SW_VERSION = "1.1.1-release-build030519"
 
 // NOTE session cookie name
 const SESSID = "SESSID"
